e1c6179dfe459c5225a91955fb76bc32bf3b2016
TranDangKhoi <88824627+TranDangKhoi@users.noreply.github.com>
2022-08-02 17:03:26 +0700

    practical exam

---
+++ pro_management/app/Models/Category.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Category extends Model
+{
+    use HasFactory;
+    public $fillable = [
+        'id',
+        'category_name',
+    ];
+
+    public function product(){
+        return $this->hasMany(product::class,'category_id','id');
+    }
+}
